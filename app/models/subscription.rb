acb6fd3f86e7fa16351ff3430a882a773e782b91
Didi Dodson <mdodson55@gmail.com>
2022-02-22 14:04:13 -0700

    Add subscription model and spec

---
+++ app/models/subscription.rb
@@ -0,0 +1,22 @@
+class Subscription < ApplicationRecord
+  belongs_to :customer
+
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :status, presence: true
+  validates :frequency, presence: true
+  validates :customer_id, presence: true
+  validates :tea_name, presence: true
+  validates :tea_description, presence: true
+  validates :temperature, numericality: true, presence: true
+  validates :brew_time, numericality: true, presence: true
+
+  enum status: {
+    active: 0,
+    cancelled: 1,
+  }
+
+  def self.by_customer(customer)
+    self.where(customer_id: customer.id)
+  end
+end
---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe Subscription, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:customer) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:frequency) }
+    it { should validate_presence_of(:customer_id) }
+    it { should validate_presence_of(:tea_name) }
+    it { should validate_presence_of(:tea_description) }
+    it { should validate_presence_of(:temperature) }
+    it { should validate_presence_of(:brew_time) }
+  end
+end
