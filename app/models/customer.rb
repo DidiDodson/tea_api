7468183a1bc133391dc59010def2108851179622
Didi Dodson <mdodson55@gmail.com>
2022-02-22 13:34:41 -0700

    Add customer model and spec

---
+++ app/models/customer.rb
@@ -0,0 +1,8 @@
+class Customer < ApplicationRecord
+  has_many :subscriptions
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :address, presence: true
+end
