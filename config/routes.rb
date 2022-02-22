d7099191164e2afe7f28e00bb45da9352a74f959
Didi Dodson <mdodson55@gmail.com>
2022-02-22 14:30:40 -0700

    Update routes file with resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      namespace :customers do
+      resources :customers , only: [:show] do 
         resources :subscriptions
       end
     end
