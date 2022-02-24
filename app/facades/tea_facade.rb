bb14ad4ac2838ef135d03281594a9c3bc9743fd5
Didi Dodson <mdodson55@gmail.com>
2022-02-24 15:03:47 -0700

    Update tea facade and spec to instatiate

--- app/facades/tea_facade.rb
+++ app/facades/tea_facade.rb
@@ -2,6 +2,8 @@ class TeaFacade
   class << self
     def find_tea(name)
       result = TeaService.get_one_tea(name)
+
+      Tea.new(result)
     end
   end
 end
--- spec/facades/tea_facade_spec.rb
+++ spec/facades/tea_facade_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe TeaFacade do
   it 'finds one tea by name' do
     name = 'black'
 
-    result = TeaService.get_one_tea(name)
+    result = TeaFacade.find_tea(name)
 
-    expect(result[:name]).to eq('black')
-    expect(result[:description]).to eq('Boosts heart health and lowers cholesterol.')
-    expect(result[:brew_time]).to eq(3)
-    expect(result[:temperature]).to eq(85)
+    expect(result.name).to eq('black')
+    expect(result.description).to eq('Boosts heart health and lowers cholesterol.')
+    expect(result.brew_time).to eq(3)
+    expect(result.temperature).to eq(85)
   end
 end
